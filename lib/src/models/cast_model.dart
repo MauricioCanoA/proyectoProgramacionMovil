24c2bf3d8666a034483ddf5133afa608516054db
Mauricio <17030487@itcelaya.edu.mx>
2021-12-10 17:41:11 -0600

    commit

--- lib/src/models/cast_model.dart
+++ lib/src/models/cast_model.dart
@@ -6,6 +6,17 @@ class CastModel{
 
   CastModel({
     this.id,
-    this.name
+    this.name,
+    this.character,
+    this.profilePath
   });
+
+  factory CastModel.fromMap(Map<String,dynamic> map){
+    return CastModel(
+      id: map['id'],
+      name: map['name'],
+      character: map['character'],
+      profilePath: map['profile_path']
+    );
+  }
 }
